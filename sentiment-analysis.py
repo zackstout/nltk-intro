1622352693cff4b6252d384aef34eefb675bbad2
Zack Stout <zackstout@gmail.com>
2018-01-29 21:54:12 -0600

    glancing over the next few tuts and taking some notes

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,2 +1,16 @@
 
-# following sentdex on NLTK vids 11-13:
+# following sentdex on NLTK vids 11-15:
+
+# we can do frequency distribution to find most common words, as well as print each instance of a word.
+
+# we can save "features" to mark words in reviews as positive or negative
+
+# this prepares us for (supervised) machine learning, with e.g. Naive Bayes classification.
+
+# we can save our classifier in a pickle (so that we needn't retrain it every time.)
+
+# NLTK actually incorporates scikit-learn! We can bring in a ton of idfferent ML algorithms (e.g. Logistic regression, SVC).
+
+# And then we can (using an algo!) test them against each other.
+
+# Mmm definitely check out his Machine learning tutorial (sci-kit-learn).
